0b7745742de044fc993ccfec3150fa31a31caf71
ROKAYA-AYOUBI <61829390+ROKAYA-AYOUBI@users.noreply.github.com>
2020-04-19 21:31:57 +0200

    Update SecurityConfig.java

--- src/main/java/com/manager/CampChildreanHolydayManager/securite/SecurityConfig.java
+++ src/main/java/com/manager/CampChildreanHolydayManager/securite/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.manager.CampChildreanHolydayManager.securite;
 
+
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,31 +12,48 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true)  
+@EnableGlobalMethodSecurity(securedEnabled = true)  
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
-	private void globalConfig(AuthenticationManagerBuilder  auth) throws Exception {
+	private void globalConfig(AuthenticationManagerBuilder  auth,DataSource dataSource) throws Exception {
 		
-		  auth.inMemoryAuthentication().withUser("admin").password("{noop}123").roles("ADMIN","Personne"); 
-		  auth.inMemoryAuthentication().withUser("personne").password("{noop}123").roles( "personne");
+	
+		  auth.inMemoryAuthentication().withUser("personne").password("{noop}123").roles( "Personne");
+		  auth.inMemoryAuthentication().withUser("admin").password("{noop}123").roles("ADMIN"); 
 		  auth.inMemoryAuthentication().withUser("economiste").password("{noop}123").roles("economiste");
 		  auth.inMemoryAuthentication().withUser("assistentmedical").password("{noop}123").roles("assistentmedical");
 		  auth.inMemoryAuthentication().withUser("moniteur").password("{noop}123").roles("moniteur");
 		 
 		
 		/*
-		 * auth.inMemoryAuthentication()
-		 * .withUser("admin").password("{noop}admin").roles("USER");
-		 */
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery("select username as principal,password as credentials ,true from users where username=?") 
+			.authoritiesByUsernameQuery("select users_username as principal,roles_role as role from users_roles where users_username=?") 
+		.rolePrefix("ROLE_") ;*/
+			//	.rolePrefix("ROLE_") .passwordEncoder(new Md5PasswordEncoder()); } 
+		
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception { 
+		http.formLogin().loginPage("/login"); 
+		http.authorizeRequests().antMatchers("/operations","/consultercompte").hasRole("US ER"); 
+		http.authorizeRequests().antMatchers("/saveOperation").hasRole("ADMIN"); 
+		http.exceptionHandling().accessDeniedPage("/403"); } }
+	- SecurityController package org.sid.sec;*/
+		
+		
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
+				.antMatchers( "/css/**","/img/**","/fonts/**","/js/**","/images/**","/vendor/**","/videos/**").permitAll()
+				.antMatchers("/", "/P-Pedaogique","/P-Fixe","/p-General").permitAll() 
 				.anyRequest()
 					.authenticated()
 						.and()
@@ -42,4 +63,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		   .defaultSuccessUrl("/")
 		   .failureUrl("/error.html");
 	}
+	
+	
+
+	
+	
+	
+	
+	
 }
